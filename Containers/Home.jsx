32bfaa2475e631b3509957977727788177931c14
George A. Michel <sarumonakuba@gmail.com>
2017-03-25 15:23:25 -0700

    Add material-ui and nested routes

--- Containers/Home.jsx
+++ Containers/Home.jsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
+import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {deepOrange500} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-export default () => (
-  <div> Bloop bloop </div>
-)
+const styles = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 200
+  }
+};
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  }
+})
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div>
+          <AppBar 
+            title='Iaso'
+          />
+          {this.props.children}
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default Home;

5d9849c96036f9567f1cdea2848b34ebc9c50848
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-03-25 15:27:45 -0700

    import statements for material-ui components

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import {render} from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
 import Home from './Containers/Home';
 import Records from './Containers/Records';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'amterial-ui/RaisedButton';
 
 render(
   <Router history={browserHistory}>

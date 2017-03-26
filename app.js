79393ae025676d7077958bf74f1c4ee2a80e396e
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-03-25 17:43:30 -0700

    adds mockup for record submission

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ import {render} from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
 import Home from './Containers/Home';
 import Records from './Containers/Records';
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
-import RaisedButton from 'amterial-ui/RaisedButton';
+import Submit from './Containers/Submit';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 render(
   <Router history={browserHistory}>
     <Route component={Home}>
-      <Route path='/' component={Records}/> 
+      <Route path='/' component={Submit}/>
     </Route>
   </Router>,
   document.getElementById('app')
